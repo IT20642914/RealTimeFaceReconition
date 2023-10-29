00f9e4fb70847e78e6befd43afa00465ef3243a8
Avishka Chanaka <It206429142@my.sliit.lk>
2023-10-29 14:07:35 +0530

    create main file and read  from file

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ python -m venv env
 
  env\Scripts\activate   
  cmake --version 
+
+ pip install dlib==19.22  
+ pip install opencv-python 
+ pip install face_recognition
   
--- main.py
+++ main.py
@@ -1,44 +1,39 @@
 import face_recognition
-import os,sys
+import os, sys
 import cv2
 import numpy as np
 import math
 
-def face_confidence(face_distance,face_match_threshold=0.6):
- range=(1.0-face_match_threshold)
- linear_val=(1.0-face_distance)/(range-2.0)
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range - 2.0)
 
- if(face_distance>face_match_threshold):
-  return str(round(linear_val*100,2))+'%'
- else:
-  value=(linear_val+((1.0-linear_val) * math.pow((linear_val-0.5)*2,0.2)))
-  return str(round(value,2)) + '%'
- 
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else:
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2)))
+        return str(round(value, 2)) + '%'
 
-class FaceRecognition :
- face_location=[]
- face_encodings=[]
- face_names=[]
- known_face_encodings=[]
- known_face_names=[]
- process_current_frame=True
+class FaceRecognition:
+    face_location = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
 
- def __init__(self) :
-  self.encode_faces()
+    def __init__(self):
+        self.encode_faces()
 
-def encode_faces(self):
- for image in os.listdir('faces'):
-   face_image=face_recognition.load_image_file(f'faces/{image}')
-   face_encoding=face_recognition.face_encodings(image)[0]
+    def encode_faces(self):
+        for image in os.listdir('faces'):
+            face = face_recognition.load_image_file("faces/" + image)
+            encoding = face_recognition.face_encodings(face)[0]
 
+            self.known_face_encodings.append(encoding)
+            self.known_face_names.append(image)
 
-   self.known_face_encodings.append(face_encoding)
-   self.known_face_names.append(image)
+        print(self.known_face_names)
 
-   print(self.known_face_names)
-
-if __name__=='__main__':
-  fr=FaceRecognition()
-  
-
-
+if __name__ == '__main__':
+    fr = FaceRecognition()
